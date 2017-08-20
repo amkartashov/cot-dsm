b417e0c493b6703645955805b7a4404c27393e00
Andrey Kartashov <gorilych@gmail.com>
2017-08-21 04:22:22 +0700

    Added view for containers

--- dsm/dckr.py
+++ dsm/dckr.py
@@ -1,8 +1,19 @@
 import docker
 
+def get_nodes():
+    client = docker.from_env()
+    return [get_node_info(node) for node in client.nodes.list()]
+
+def get_containers(node_id):
+    client = docker.from_env()
+    node = client.nodes.get(node_id)
+    nodedc = docker.DockerClient(base_url='tcp://' + node.attrs['Status']['Addr'] +':2375')
+    return [get_ct_info(ct) for ct in nodedc.containers.list()]
+
 def get_node_info(node):
     info = { 
         "ID": node.attrs['ID'],
+        "short_id": node.short_id,
         "Hostname": node.attrs['Description']['Hostname'],
         "MemoryBytes": node.attrs['Description']['Resources']['MemoryBytes'],
         "NanoCPUs": node.attrs['Description']['Resources']['NanoCPUs'],
@@ -15,9 +26,15 @@ def get_node_info(node):
     info['ContainersRunning'] = nodedc.info()['ContainersRunning']
     return info
 
-def get_nodes():
-    client = docker.from_env()
-    return [get_node_info(node) for node in client.nodes.list()]
-
-def get_containers():
-    return None
+def get_ct_info(ct):
+    info = {
+        "short_id": ct.short_id,
+        "service_name": ct.attrs['Config']['Labels']['com.docker.swarm.service.name'],
+        "service_id": ct.attrs['Config']['Labels']['com.docker.swarm.service.id'],
+        "task_name": ct.attrs['Config']['Labels']['com.docker.swarm.task.name'],
+        "mem_limit": ct.attrs['HostConfig']['Memory'],
+        "ioweight": ct.attrs['HostConfig']['BlkioWeight'],
+        "cpushares": ct.attrs['HostConfig']['CpuShares'],
+        "status": ct.attrs['State']['Status'],
+    }
+    return info()
